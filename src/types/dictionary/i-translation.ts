7f4cc4920d0087113abb8af1246577f0f822ffea
John Adib <mr.john.adib@gmail.com>
2025-01-19 03:12:42 +0000

    Enhance ITranslation interface by adding new language properties (Arabic, Bengali, Portuguese, Russian, Urdu, German, Italian) with speaker counts for improved multilingual support.

--- src/types/dictionary/i-translation.ts
+++ src/types/dictionary/i-translation.ts
@@ -8,6 +8,20 @@ export interface ITranslation {
     es?: string;
     /** @description 4. French - 270 million speakers */
     fr?: string;
-    /** @description xx. Persian - 80 million speakers */
+    /** @description 5. Arabic - 270 million speakers   */
+    ar?: string;
+    /** @description 6. Bengali - 270 million speakers */
+    bn?: string;
+    /** @description 7. Portuguese - 260 million speakers */
+    pt?: string;
+    /** @description 8. Russian - 250 million speakers */
+    ru?: string;
+    /** @description 9. Urdu - 230 million speakers */
+    ur?: string;
+    /** @description 10. German - 135 million speakers */
+    de?: string;
+    /** @description 22. Persian - 80 million speakers */
     fa?: string;
+    /** @description 23. Italian - 68 million speakers */
+    it?: string;
   }
