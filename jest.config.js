46a4475ab200c514437d0ee3276043a9e39d13e8
John Adib <mr.john.adib@gmail.com>
2025-02-02 02:15:52 +0000

    Refactor package.json and tsconfig.json

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-  preset: 'ts-jest',
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: {
+    '^.+.tsx?$': ['ts-jest', {}],
+  },
 };
