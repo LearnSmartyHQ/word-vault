f159974fdef802c34d45ea565f4adbbd014240b8
John Adib <mr.john.adib@gmail.com>
2025-02-02 01:10:13 +0000

    Update package.json version to 3.2.0 Refactor file paths in findCard function to include subdirectories based on the first two digits of the ID Update tsconfig.json module to ESNext

--- src/services/const.ts
+++ src/services/const.ts
@@ -1,4 +1,8 @@
-import { join } from "path";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const baseCollectionPath = join(__dirname, '../../data/collections');
 export const baseCardPath = join(__dirname, '../../data/cards');
